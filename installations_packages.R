7cc2becd36e34a48fc0f27de3082ad97d5367a86
antuki <kim.demarty@laposte.net>
2023-02-10 19:05:26 +0000

    adaptation grossiere contenu suite à point du 10/02

--- installations_packages.R
+++ installations_packages.R
@@ -1,9 +1,11 @@
+
 # Packages du CRAN
 packages <- c("dplyr", "tidygeocoder", "mapview", "sf", "osmdata",
               "RColorBrewer", "ggplot2", "readr",
-              "ggspatial", "knitr", "sfnetworks", "tidygraph", "remotes")
+              "ggspatial", "knitr", "sfnetworks", "tidygraph", "remotes", "rmdformats")
 
 install.packages(setdiff(packages, rownames(installed.packages()))) 
 
 # Packages hors CRAN
 remotes::install_github("joelgombin/banR")
+devtools::install_github("koncina/unilur")
