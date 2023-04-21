922fe3246f7e60f377e9f4c3c4aac3b2ed0a334f
antuki <kim.antunez@ensae.fr>
2023-04-21 20:13:22 +0200

    debug intégration continue : installation de tidygeocoder ?

--- installations_packages.R
+++ installations_packages.R
@@ -3,9 +3,8 @@
 packages <- c("dplyr", "tidygeocoder", "mapview", "sf", "osmdata",
               "RColorBrewer", "ggplot2", "readr",
               "ggspatial", "knitr", "sfnetworks", "tidygraph", "remotes", "rmdformats")
-
 install.packages(setdiff(packages, rownames(installed.packages()))) 
 
 # Packages hors CRAN
 remotes::install_github("joelgombin/banR")
-devtools::install_github("koncina/unilur")
+#remotes::install_github("koncina/unilur")
